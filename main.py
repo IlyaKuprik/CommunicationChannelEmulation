4f2640ca3436496502ab64b691e5d07e7f7bb55b
Илья <ilyasolonitsyn@mail.ru>
2022-05-06 00:21:36 +0300

    сломался get_control_bits_data. нужно починить.

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from utils.hemming_codes import Hemming
 
 
-source = "German, privet!!"
+source = "D="
 
-coder = Hemming(chunk_length=16)
+coder = Hemming(len(source))
 encoded_source = coder.encode(source)
 print(f"Строка, поданая на вход: {source}")
-print(f"Полученный код:{encoded_source}")
-print(f"Декодированный код {coder.decode(encoded_source)}")
+print(f"Полученный код: {encoded_source}")
+# сделаем ошибку
+ind = 6
+encoded_source = encoded_source[:ind] + str(int(not int(encoded_source[ind]))) + encoded_source[ind+1:]
+print(f"Декодированный код: {coder.decode(encoded_source)}")
+
+# print(chr(int('01000100', 2)), chr(int('00111101', 2)))
+
+
--- utils/hemming_codes.py
+++ utils/hemming_codes.py
@@ -1,16 +1,17 @@
+import copy
+
+
 class Hemming:
 
-    def __init__(self, chunk_length=8):
-        self.chunk_length = chunk_length  # задаём длину блока кодирования
-        self.control_bits = [i for i in range(1, self.chunk_length + 1) if not i & (i - 1)]  # контрольные биты,
+    def __init__(self, msg_len=None):
+        self.length = 8  # задаём длину одного символа
+        self.control_bits = [i for i in range(1, msg_len * self.length + 1) if not i & (i - 1)]  # контрольные биты,
         # являются степенями двойки
 
     def chars_to_bin(self, chars) -> str:
         """
         Преобразование символов в бинарный формат
         """
-        assert not len(chars) * 8 % self.chunk_length, 'Длина кодируемых данных должна быть кратна длине блока ' \
-                                                       'кодирования '
         return ''.join([bin(ord(c))[2:].zfill(8) for c in chars])
 
     @staticmethod
@@ -22,21 +23,33 @@ class Hemming:
             if not i % chunk_size:
                 yield text_bin[i:i + chunk_size]
 
-    def get_control_bits_data(self, value_bin):
+    def set_empty_control_bits(self, value_bin):
         """
-        Получение информации о контрольных битах из бинарного блока данных
+        Добавить в бинарный блок "пустые" контрольные биты
         """
-        check_bits_count_map = {k: 0 for k in self.control_bits}
-        for index, value in enumerate(value_bin, 1):
-            if int(value):
-                bin_char_list = list(bin(index)[2:].zfill(8))
-                bin_char_list.reverse()
-                for degree in [2 ** int(i) for i, value in enumerate(bin_char_list) if int(value)]:
-                    check_bits_count_map[degree] += 1
-        check_bits_value_map = {}
-        for check_bit, count in check_bits_count_map.items():
-            check_bits_value_map[check_bit] = 0 if not count % 2 else 1
-        return check_bits_value_map
+        for bit in self.control_bits:
+            value_bin = value_bin[:bit - 1] + '0' + value_bin[bit - 1:]
+        return value_bin
+
+    def get_control_bits_data(self, value_bin) -> str:
+        """
+        Получение информации о контрольных битах из бинарных данных
+        На входе бинарная строка без контрольных бит
+        На выходе строка с уже заполненными битами
+        """
+
+        value_bin = self.set_empty_control_bits(value_bin)
+        print(value_bin)
+        for index in self.control_bits:
+            bits_sum = 0
+            for i in range(index - 1, len(value_bin), index + 1):
+                bits_sum += sum([int(value_bin[j]) for j in range(i, min(i + index, len(value_bin)))])
+            # TODO: предыдущие 2 строчки нихуя не работают. переделать.
+            print(index, bits_sum)
+            if not (bits_sum % 2):
+                value_bin = value_bin[:index] + '1' + value_bin[index + 1:]
+        print(value_bin)
+        return value_bin
 
     def set_empty_control_bits(self, value_bin):
         """
@@ -50,12 +63,9 @@ class Hemming:
         """
         Установить значения контрольных бит
         """
-        value_bin = self.set_empty_control_bits(value_bin)
-        check_bits_data = self.get_control_bits_data(value_bin)
-        for check_bit, bit_value in check_bits_data.items():
-            value_bin = '{0}{1}{2}'.format(
-                value_bin[:check_bit - 1], bit_value, value_bin[check_bit:])
-        return value_bin
+
+        source_with_bits = self.get_control_bits_data(value_bin)
+        return source_with_bits
 
     def encode(self, source, is_bin=False) -> str:
         """
@@ -64,10 +74,8 @@ class Hemming:
         text_bin = source
         if not is_bin:
             text_bin = self.chars_to_bin(source)
-        result = ''
-        for chunk_bin in self.chunk_iterator(text_bin, self.chunk_length):
-            chunk_bin = self.set_control_bits(chunk_bin)
-            result += chunk_bin
+        # print(f"text_bin: {text_bin}")
+        result = self.set_control_bits(text_bin)
         return result
 
     def exclude_control_bits(self, value_bin):
@@ -81,33 +89,32 @@ class Hemming:
 
         return clean_value_bin
 
-    def check_and_fix_error(self, encoded_chunk):
+    def check_and_fix_error(self, encoded_source):
         """
-        Проверка и исправление ошибки в блоке бинарных данных
+        Проверка и исправление ошибки в бинарной строке
         """
         # TODO: алгоритм проверки ошибок
-        return encoded_chunk
+        encoded_without_bits = self.exclude_control_bits(encoded_source)
+        new_encoded_source = self.get_control_bits_data(encoded_without_bits)
+        err_bit = 0
+        for index in self.control_bits:
+            if new_encoded_source[index - 1] != encoded_source[index - 1]:
+                err_bit += index
+        return encoded_source
 
-    def decode(self, encoded, fix_errors=True):
+    def decode(self, encoded):
         """
         Декодирование данных
         """
         decoded_value = ''
-        fixed_encoded_list = []
-        # разделение на блоки для декодирования и исправления ошибок
-        for encoded_chunk in self.chunk_iterator(encoded, self.chunk_length + len(self.control_bits)):
-            if fix_errors:
-                encoded_chunk = self.check_and_fix_error(encoded_chunk)
-            fixed_encoded_list.append(encoded_chunk)
+
+        # исправление ошибок
+        fixed_encoded = self.check_and_fix_error(encoded)
 
         # удаление контрольных битов из сообщения
-        clean_chunk_list = []
-        for encoded_chunk in fixed_encoded_list:
-            encoded_chunk = self.exclude_control_bits(encoded_chunk)
-            clean_chunk_list.append(encoded_chunk)
+        fixed_encoded = self.exclude_control_bits(fixed_encoded)
 
         # декодирование бинарных блоков и объединение в одно сообщение
-        for clean_chunk in clean_chunk_list:
-            for clean_char in [clean_chunk[i:i + 8] for i in range(len(clean_chunk)) if not i % 8]:
-                decoded_value += chr(int(clean_char, 2))
+        for clean_char in [fixed_encoded[i:i + 8] for i in range(len(fixed_encoded)) if not i % 8]:
+            decoded_value += chr(int(clean_char, 2))
         return decoded_value
